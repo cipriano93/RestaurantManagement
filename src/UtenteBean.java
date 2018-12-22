b549230d33ed1e963f36927ea1c417b7494766af
Andrea <a.cipriano17@studenti.unisa.it>
2018-12-22 14:01:21 +0100

    Iniziamo?

---
+++ src/UtenteBean.java
@@ -0,0 +1,14 @@
+
+public class UtenteBean {
+
+	public UtenteBean() {}
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	String username;
+}
