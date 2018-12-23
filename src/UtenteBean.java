f0ca4b55f153dc65e432086acdbf2160bb6bfcf0
Andrea <a.cipriano17@studenti.unisa.it>
2018-12-23 02:06:28 +0100

    iniziamo?

--- src/UtenteBean.java
+++ src/UtenteBean.java
@@ -11,19 +11,7 @@ public class UtenteBean {
 	}
 
 	
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getCognome() {
-		return cognome;
-	}
-	public void setCognome(String cognome) {
-		this.cognome = cognome;
-	}
 
-
+X
 	String username, nome, cognome;
 }
