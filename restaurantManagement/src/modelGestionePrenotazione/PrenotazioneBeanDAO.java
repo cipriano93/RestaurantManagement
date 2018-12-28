72eacc84858499ce66556e762d56d1510869915e
usain97 <M.FLORA4@studenti.unisa.it>
2018-12-28 12:03:48 +0100

    prenotazione bean dao

--- restaurantManagement/src/modelGestionePrenotazione/PrenotazioneBeanDAO.java
+++ restaurantManagement/src/modelGestionePrenotazione/PrenotazioneBeanDAO.java
@@ -1,5 +1,41 @@
 package modelGestionePrenotazione;
 
-public class PrenotazioneBeanDAO {
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import modelConnection.DriverManagerConnectionPool;
+import modelGestioneMenù.MenùBean;
+
+public class PrenotazioneBeanDAO {
+	
+	public synchronized  boolean doSave(PrenotazioneBean pb, String username) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		try {
+			con = DriverManagerConnectionPool.getConnection();
+			if ((pb.getDescrizione())==null)
+				ps = con.prepareStatement("INSERT INTO menu VALUES (?, ?, ?, ?)");
+			else
+				ps = con.prepareStatement("INSERT INTO menu VALUES (?, ?, ?, ?, ?)");
+			ps.setInt(1, pb.getIdPrenotazione());
+			ps.setString(2, username);
+			Date date = new Date(pb.getData().getTimeInMillis());
+			ps.setDate(3, date);
+			ps.executeUpdate();
+			con.commit();
+			return true;
+		} catch (SQLException e) {
+			return false;
+		} finally {
+			try {
+				DriverManagerConnectionPool.releaseConnection(con);
+				ps.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
