ed7057197e527f0ef1fad54138e8e210fee71f4f
usain97 <M.FLORA4@studenti.unisa.it>
2018-12-28 13:56:38 +0100

    attività bean

--- restaurantManagement/src/modelGestioneAttività/AttivitàBean.java
+++ restaurantManagement/src/modelGestioneAttività/AttivitàBean.java
@@ -2,4 +2,64 @@ package modelGestioneAttività;
 
 public class AttivitàBean {
 
+	public String getNome() {
+		return nome;
+	}
+	
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+	
+	
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+
+
+	public String getPIva() {
+		return p_iva;
+	}
+
+
+	public void setPIva(String p_iva) {
+		this.p_iva = p_iva;
+	}
+
+	public String getProvincia() {
+		return provincia;
+	}
+	
+	
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+
+	
+	public String getCittà() {
+		return città;
+	}
+
+
+	public void setCittà(String città) {
+		this.città = città;
+	}
+	
+	
+	public String getTel() {
+		return tel;
+	}
+
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+
+	private String nome, indirizzo, p_iva, provincia, città, tel;
 }
