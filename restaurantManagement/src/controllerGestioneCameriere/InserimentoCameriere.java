087190fe0fa0faf392157ccf144e110efc63a684
Andrea <a.cipriano17@studenti.unisa.it>
2018-12-29 12:58:14 +0100

    gestione cameriere(servlet and manager)

--- restaurantManagement/src/controllerGestioneCameriere/InserimentoCameriere.java
+++ restaurantManagement/src/controllerGestioneCameriere/InserimentoCameriere.java
@@ -7,34 +7,58 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import modelGestioneCameriere.CameriereManager;
+import modelGestioneUtente.UtenteManager;
+
 /**
  * Servlet implementation class InserimentoCameriere
  */
 @WebServlet("/InserimentoCameriere")
 public class InserimentoCameriere extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public InserimentoCameriere() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+private static final long serialVersionUID = 1L;
+    
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String nome = request.getParameter("name");
+		String cognome = request.getParameter("surname");
+		String username = request.getParameter("usr");
+		String password = request.getParameter("pwd");
+		String confirmPassword = request.getParameter("pwd_confirm");
+		
+		String regexUsername = "^[0-9a-zA-Z]+$";
+		
+		
+		CameriereManager cm = new CameriereManager();
+		UtenteManager um = new UtenteManager();
+		
+		//questo è un controllo più accurato, se l'email non rispetta il pattern, esce direttamente
+		if(username.matches(regexUsername) && password.equals(confirmPassword)
+									&& (um.login(username, password)) == null) {
+
+			
+			boolean result = cm.inserimentoCameriere(username, password, nome, cognome, "cameriere");
+		
+			if(result) {
+				response.sendRedirect("login.jsp");
+			} else {
+				request.setAttribute("errMessage", result);
+				request.getRequestDispatcher("registrazione.jsp").forward(request, response);	
+			}
+		} else {
+			response.sendRedirect("registrazione.jsp");
+		}	
 	}
 
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
--- restaurantManagement/src/controllerGestioneCameriere/ModificaCameriere.java
+++ restaurantManagement/src/controllerGestioneCameriere/ModificaCameriere.java
@@ -7,34 +7,59 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import modelGestioneCameriere.CameriereManager;
+import modelGestioneUtente.UtenteBeanDAO;
+import modelGestioneUtente.UtenteManager;
+
 /**
  * Servlet implementation class ModificaCameriere
  */
 @WebServlet("/ModificaCameriere")
 public class ModificaCameriere extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ModificaCameriere() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+    
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String username = request.getParameter("username");
+		String name = request.getParameter("name");
+		String surname = request.getParameter("surname");
+		String password = request.getParameter("pwd");
+		String confirmPassword = request.getParameter("pwd_confirm");
+		
+		//prima della creazione controllo sulla chiave
+		UtenteBeanDAO ubd = new UtenteBeanDAO();
+		String regexName = "^[A-Za-z\\s]{3,}$";
+		String regexSurname ="^[A-Za-z\\s]{3,}$";
+		String regexPassword = "^(?=.*[0-9])(?=.*[A-Z]).{5,}$";
+		
+		//questo è un controllo più accurato, se l'email non rispetta il pattern, esce direttamente
+		if(password.equals(confirmPassword) && password.matches(regexPassword) && name.matches(regexName) && surname.matches(regexSurname)) {
+			//Creazione utente manager
+			CameriereManager cm = new CameriereManager();
+		
+			boolean result = cm.modificaCameriere(username, password, name, surname, "cameriere");
+			
+			if(result) {
+				response.sendRedirect("areaPersonaleCliente.jsp");
+			} else {
+				request.setAttribute("errMessage", result);
+				request.getRequestDispatcher("modificaDatiPersonali.jsp").forward(request, response);	
+			}
+		} else {
+			response.sendRedirect("modificaDatiPersonali.jsp");
+		}	
 	}
 
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
