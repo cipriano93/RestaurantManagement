53a3b3121f305a0b640487e2eab846024209ec87
Andrea <a.cipriano17@studenti.unisa.it>
2019-01-21 12:07:47 +0100

    test start

--- restaurantManagement/src/testingUtente/TestDoSave.java
+++ restaurantManagement/src/testingUtente/TestDoSave.java
@@ -8,16 +8,14 @@ import org.junit.jupiter.api.Test;
 import model.UtenteBean;
 import model.UtenteBeanDAO;
 
-class TestUtenteBeanDAO {
+class TestDoSave {
 	UtenteBean ub = new UtenteBean(); 
-	UtenteBean ub1 = new UtenteBean();
-	
 	UtenteBeanDAO ubd = new UtenteBeanDAO();	
-	
+
 	@Test
 	public void doSaveTest() {
-		ub.setUsername("gestore91111");
-		ub.setPassword("gestore93");
+		ub.setUsername("gestore97");
+		ub.setPassword("gestore97");
 		ub.setNome("andrea");
 		ub.setCognome("cipriano");
 		ub.setTipo("gestore");
@@ -40,18 +38,18 @@ class TestUtenteBeanDAO {
 	
 	@Test
 	public void doUpdateTest() {
-		ub1.setUsername("cameriere97");
-		ub1.setPassword("cameriere97");
-		ub1.setNome("Giuseppe");
-		ub1.setCognome("rotondo97");
-		ub1.setTipo("cameriere");
+		ub.setUsername("cameriere97");
+		ub.setPassword("cameriere97");
+		ub.setNome("Giuseppe");
+		ub.setCognome("rotondo97");
+		ub.setTipo("cameriere");
 		assertEquals(true, ubd.doUpdate(ub));
 	}
-	
+	/*
 	@Test
 	public void doDeleteTest() {
-		assertEquals(true, ubd.doDelete("gestore91111"));
+		assertEquals(true, ubd.doDelete("gestore89"));
 	}
-	
+	*/
 	
 }
--- restaurantManagement/src/testingUtente/UtenteTestSuite.java
+++ restaurantManagement/src/testingUtente/UtenteTestSuite.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-	TestUtenteBeanDAO.class
+	TestDoSave.class
 })
 public class UtenteTestSuite {
 	
